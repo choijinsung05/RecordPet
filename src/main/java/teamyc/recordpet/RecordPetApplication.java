ae4c7e54b9cee63dd428398575f3c34a7b98992a
jinsung <sanoi12@naver.com>
2024-12-16 14:12:14 +0900

    Initial commit

---
+++ src/main/java/teamyc/recordpet/RecordPetApplication.java
@@ -0,0 +1,13 @@
+package teamyc.recordpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordPetApplication.class, args);
+	}
+
+}
