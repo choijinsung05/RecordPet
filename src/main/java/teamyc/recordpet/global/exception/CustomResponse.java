1192a7c2e848e6935bb9f205fd0f676bc57104e6
yongcowhee <yongcowhee@gmail.com>
2024-12-19 02:52:35 +0900

    feat: CustomResponse 구현

---
+++ src/main/java/teamyc/recordpet/global/exception/CustomResponse.java
@@ -0,0 +1,33 @@
+package teamyc.recordpet.global.exception;
+
+import java.io.Serializable;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class CustomResponse<T> implements Serializable {
+
+    private HttpStatus httpStatus;
+    private String code;
+    private String message;
+    private T data;
+
+    public static <T> CustomResponse<T> success(T data) {
+        return CustomResponse.<T>builder()
+            .httpStatus(ResultCode.SUCCESS.getHttpStatus())
+            .code(ResultCode.SUCCESS.getCode())
+            .message(ResultCode.SUCCESS.getMessage())
+            .data(data)
+            .build();
+    }
+
+    public static <T> CustomResponse<T> error(ResultCode resultCode) {
+        return CustomResponse.<T>builder()
+            .httpStatus(resultCode.getHttpStatus())
+            .code(resultCode.getCode())
+            .message(resultCode.getMessage())
+            .build();
+    }
+}
