186456c8c876c6da0f5b6ef78c422915c0666778
yongcowhee <yongcowhee@gmail.com>
2024-12-24 17:27:58 +0900

    feat: 회원가입DTO validation 추가

--- src/main/java/teamyc/recordpet/domain/user/controller/UserController.java
+++ src/main/java/teamyc/recordpet/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package teamyc.recordpet.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public CustomResponse<UserSignupResponse> signup(@RequestBody UserSignupRequest req) {
+    public CustomResponse<UserSignupResponse> signup(@RequestBody @Valid UserSignupRequest req) {
         return CustomResponse.success(userService.signup(req));
     }
 }
--- src/main/java/teamyc/recordpet/domain/user/dto/UserSignupRequest.java
+++ src/main/java/teamyc/recordpet/domain/user/dto/UserSignupRequest.java
@@ -1,16 +1,21 @@
 package teamyc.recordpet.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import teamyc.recordpet.domain.user.entity.User;
 import teamyc.recordpet.domain.user.entity.Role;
+import teamyc.recordpet.domain.user.entity.User;
 
 @Getter
 @AllArgsConstructor
 public class UserSignupRequest {
 
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")
     private final String email;
+    @NotBlank
     private final String nickname;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>])(?=.*[a-zA-Z]).{1,16}$\n")
     private final String password;
 
     public User toEntity(String encodedPassword) {
