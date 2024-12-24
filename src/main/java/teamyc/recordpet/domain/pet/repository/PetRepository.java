24e00a897bdaed618d5715dd62280c54c970ad73
jinsung <sanoi12@naver.com>
2024-12-24 18:15:50 +0900

    refactor : @RepositoryDefinition 사용하여 메서드 제한

--- src/main/java/teamyc/recordpet/domain/pet/repository/PetRepository.java
+++ src/main/java/teamyc/recordpet/domain/pet/repository/PetRepository.java
@@ -1,7 +1,21 @@
 package teamyc.recordpet.domain.pet.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.RepositoryDefinition;
 import teamyc.recordpet.domain.pet.entity.Pet;
 
-public interface PetRepository extends JpaRepository<Pet, Long> {
+import java.util.List;
+import java.util.Optional;
+
+@RepositoryDefinition(domainClass = Pet.class, idClass = Long.class)
+public interface PetRepository {
+
+    Pet save(Pet pet);
+
+    Optional<Pet> findById(Long id);
+
+    List<Pet> findAll();
+
+    void deleteById(long id);
+
+    void deleteAll();
 }
