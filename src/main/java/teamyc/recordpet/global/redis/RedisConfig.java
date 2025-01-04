b1bcb7ffa93727b7af5dbb6168770ba215a1564d
yongcowhee <yongcowhee@gmail.com>
2025-01-04 13:00:02 +0900

    feat: redis config 추가

---
+++ src/main/java/teamyc/recordpet/global/redis/RedisConfig.java
@@ -0,0 +1,34 @@
+package teamyc.recordpet.global.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisConfig {
+
+    private final String host;
+    private final int port;
+
+    public RedisConfig(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
