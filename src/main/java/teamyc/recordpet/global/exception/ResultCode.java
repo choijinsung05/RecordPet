91edf8bf581fbf7a337ddba7d5f5ab68521eff5c
yongcowhee <yongcowhee@gmail.com>
2025-01-07 22:46:52 +0900

    feat: MailUtil 삭제 후 Service 통합

--- src/main/java/teamyc/recordpet/global/exception/ResultCode.java
+++ src/main/java/teamyc/recordpet/global/exception/ResultCode.java
@@ -17,6 +17,7 @@ public enum ResultCode {
     EMAIL_SEND_FAILED("UE001", "이메일 전송에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_CODE("UE002", "인증 코드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
     NOT_FOUND_EMAIL_AUTH("UE003", "인증 정보가 존재하지 않습니다. 다시 시도하세요.", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED_EMAIL("UE004", "인증 완료되지 않은 email 입니다.", HttpStatus.UNAUTHORIZED),
 
     // Pet P-
     NOT_FOUND_PET_PROFILE("P001", "조회된 프로필이 없습니다.", HttpStatus.NOT_FOUND);
--- src/main/java/teamyc/recordpet/global/mail/service/EmailAuthService.java
+++ src/main/java/teamyc/recordpet/global/mail/service/EmailAuthService.java
@@ -1,8 +1,16 @@
 package teamyc.recordpet.global.mail.service;
 
+import static teamyc.recordpet.global.exception.ResultCode.EMAIL_SEND_FAILED;
+import static teamyc.recordpet.global.exception.ResultCode.INVALID_CODE;
 import static teamyc.recordpet.global.exception.ResultCode.NOT_FOUND_EMAIL_AUTH;
 
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import teamyc.recordpet.global.exception.GlobalException;
 import teamyc.recordpet.global.mail.entity.EmailAuth;
@@ -13,21 +21,64 @@ import teamyc.recordpet.global.mail.repository.EmailAuthRepository;
 public class EmailAuthService {
 
     private final EmailAuthRepository emailAuthRepository;
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String email;
+
+    @Value("${spring.mail.url.auth}")
+    private String authEmailLink;
+
+    public String createAuthCode() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public void sendMessage(String to) {
+        try {
+            String code = createAuthCode();
+            MimeMessage message = createMessage(to, code);
+
+            if (emailAuthRepository.existsById(to)) {
+                emailAuthRepository.deleteById(to);
+            }
+            EmailAuth emailAuth = EmailAuth.builder().email(to).code(code).build();
+
+            emailAuthRepository.save(emailAuth);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new GlobalException(EMAIL_SEND_FAILED);
+        }
+    }
+
+    public void checkCode(String email, String code) {
+        EmailAuth emailAuth = emailAuthRepository.findById(email)
+            .orElseThrow(() -> new GlobalException(NOT_FOUND_EMAIL_AUTH));
+
+        if (!code.equals(emailAuth.getCode())) {
+            throw new GlobalException(INVALID_CODE);
+        }
+
+        emailAuthRepository.deleteById(email);
+        EmailAuth newEmailAuth = EmailAuth.builder().email(email).code(code).isChecked(true)
+            .build();
+
+        emailAuthRepository.save(newEmailAuth);
+    }
+
+    private MimeMessage createMessage(String to, String code)
+        throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        message.setFrom(email);
+        message.addRecipients(RecipientType.TO, to);
+        message.setSubject("[RecordPet] 이메일 인증");
+        message.setText(authEmailLink + "?email=" + to + "&authCode=" + code);
+
+        return message;
+    }
 
     public EmailAuth findById(String email) {
         return emailAuthRepository.findById(email)
             .orElseThrow(() -> new GlobalException(NOT_FOUND_EMAIL_AUTH));
-    }
-
-    public boolean hasMail(String email) {
-        return emailAuthRepository.existsById(email);
-    }
-
-    public void save(EmailAuth emailAuth) {
-        emailAuthRepository.save(emailAuth);
-    }
-
-    public void delete(String email) {
-        emailAuthRepository.deleteById(email);
     }
 }
