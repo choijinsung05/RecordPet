4f5a7240f58e726c3961c74da6ce09870396f533
jinsung <sanoi12@naver.com>
2025-01-14 14:03:05 +0900

    refactor : ResponseEntity -> CustomResponse 변경
    반환값에 대해서 customResponse 적용

--- src/main/java/teamyc/recordpet/global/s3/S3Controller.java
+++ src/main/java/teamyc/recordpet/global/s3/S3Controller.java
@@ -1,11 +1,11 @@
 package teamyc.recordpet.global.s3;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import teamyc.recordpet.global.exception.CustomResponse;
 
 @RestController
 @RequestMapping("/api/v1/s3")
@@ -18,12 +18,9 @@ public class S3Controller {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
-        try {
-            String fileUrl = s3Service.uploadFile(file, "uploads");
-            return ResponseEntity.ok("정상적으로 파일이 업로드 되었습니다. "+ fileUrl);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("업로드에 실패했습니다." + e.getMessage());
-        }
+    public CustomResponse<String> upload(@RequestParam("file") MultipartFile file) {
+
+        String fileUrl = s3Service.uploadImage(file, "uploads");
+        return CustomResponse.success(fileUrl);
     }
 }
