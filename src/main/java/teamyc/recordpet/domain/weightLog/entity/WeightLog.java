ace685a2b471f673c3c62e49ad64ab4d8e6b1a24
jinsung <sanoi12@naver.com>
2025-01-14 18:28:32 +0900

    feat : 체중을 관리하기위한 엔티티 생성 #25

---
+++ src/main/java/teamyc/recordpet/domain/weightLog/entity/WeightLog.java
@@ -0,0 +1,26 @@
+package teamyc.recordpet.domain.weightLog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import teamyc.recordpet.domain.pet.entity.Pet;
+
+import java.util.Date;
+
+@Entity
+@Getter
+public class WeightLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    private Double weight;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new Date();
+
+}
