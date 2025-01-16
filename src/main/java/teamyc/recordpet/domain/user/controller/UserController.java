9e94af20942df01b6ccdd7644c76906f9cada6af
SOHEE-YONG <yongcowhee@gmail.com>
2025-01-16 23:31:28 +0900

    feat: User 비밀번호 변경 Controller 구현 #21

--- src/main/java/teamyc/recordpet/domain/user/controller/UserController.java
+++ src/main/java/teamyc/recordpet/domain/user/controller/UserController.java
@@ -3,11 +3,15 @@ package teamyc.recordpet.domain.user.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import teamyc.recordpet.domain.user.dto.UserChangePasswordRequest;
+import teamyc.recordpet.domain.user.dto.UserChangePasswordResponse;
 import teamyc.recordpet.domain.user.dto.UserSignupRequest;
 import teamyc.recordpet.domain.user.dto.UserSignupResponse;
 import teamyc.recordpet.domain.user.service.UserService;
@@ -37,5 +41,11 @@ public class UserController {
     public CustomResponse<ConfirmMailResponse> confirmMail(
         @RequestParam(name = "email") String email, @RequestParam(name = "authCode") String code) {
         return CustomResponse.success(userService.confirmMail(email, code));
+    }
+
+    @PatchMapping("/password/{userId}")
+    public CustomResponse<UserChangePasswordResponse> changePassword(@PathVariable Long userId,
+        UserChangePasswordRequest req) {
+        return CustomResponse.success(userService.changePassword(userId, req));
     }
 }
