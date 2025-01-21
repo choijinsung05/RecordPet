d422c6f3d447fa84dd0c6930633d9f68d7e0a485
jinsung <sanoi12@naver.com>
2025-01-21 17:48:21 +0900

    refactor : response 에 record 도입

--- src/main/java/teamyc/recordpet/domain/weightLog/dto/WeightLogResponse.java
+++ src/main/java/teamyc/recordpet/domain/weightLog/dto/WeightLogResponse.java
@@ -1,18 +1,13 @@
 package teamyc.recordpet.domain.weightLog.dto;
 
 import lombok.Builder;
-import lombok.Getter;
 import teamyc.recordpet.domain.weightLog.entity.WeightLog;
 
 import java.time.LocalDate;
 
 @Builder
-@Getter
-public class WeightLogResponse {
-    private LocalDate date;
-    private Double weight;
-
-    public static WeightLogResponse fromEntity(WeightLog weightLog){
+public record WeightLogResponse(LocalDate date, Double weight) {
+    public static WeightLogResponse fromEntity(WeightLog weightLog) {
         return WeightLogResponse.builder()
                 .date(weightLog.getDate())
                 .weight(weightLog.getWeight())
