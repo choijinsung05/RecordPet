91d87c25e4001200d5ee1c8bca31e178579a65d3
jinsung <sanoi12@naver.com>
2025-01-21 17:47:52 +0900

    chore : 불필요한 공백 제거 및 코드 정렬

--- src/main/java/teamyc/recordpet/domain/weightLog/dto/WeightLogRegisterRequest.java
+++ src/main/java/teamyc/recordpet/domain/weightLog/dto/WeightLogRegisterRequest.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record WeightLogRegisterRequest(
-        @NotNull(message = "애완동물 식별자는 필수 요소입니다.") Long petId,
-        @NotNull(message = "몸무게는 필수 입력값입니다.") @Min(value = 0, message = "몸무게는 0보다 큰 값을 입력해야 합니다.") Double weight,
-        LocalDate date) {
+    @NotNull(message = "애완동물 식별자는 필수 요소입니다.") Long petId,
+    @NotNull(message = "몸무게는 필수 입력값입니다.") @Min(value = 0, message = "몸무게는 0보다 큰 값을 입력해야 합니다.") Double weight,
+    LocalDate date) {
 
     public WeightLogRegisterRequest(Long petId, Double weight, LocalDate date) {
         this.petId = petId;
